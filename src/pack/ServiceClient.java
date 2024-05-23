a523d2b0fe6f8bb149933f764564cff808ef4218
Lyna Ouabel <88051282+lynlyna04@users.noreply.github.com>
2024-05-23 12:16:42 +0100

    Update ServiceClient.java

--- src/pack/ServiceClient.java
+++ src/pack/ServiceClient.java
@@ -1,43 +1,28 @@
 package pack;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ServiceClient {
     private List<Client> clients;
 
-    public ServiceClient() {
-        this.clients = new ArrayList<>();
-    }
-
-    public List<Client> getTousLesClients() {
-        return clients;
+    public ServiceClient(List<Client> clients) {
+        this.clients = clients;
     }
 
     public Client getClientParId(int id) {
-        return clients.stream()
-                .filter(c -> c.getID() == id)
-                .findFirst()
-                .orElse(null);
+        for (Client client : clients) {
+            if (client.getID() == id) {
+                return client;
+            }
+        }
+        return null; // Or throw an exception
     }
 
-    public void ajouterClient(Client client) {
+    public void ajoutClient(Client client) {
         clients.add(client);
     }
 
-    public void modifierClient(Client client) {
-        Client clientExistant = getClientParId(client.getID());
-        if (clientExistant != null) {
-            clientExistant.setNom(client.getNom());
-            clientExistant.setPrenom(client.getPrenom());
-            clientExistant.setAdresse(client.getAdresse());
-            clientExistant.setTelephone(client.getTelephone());
-            clientExistant.setEmail(client.getEmail());
-            clientExistant.setTypeC(client.getTypeC());
-        }
-    }
-
-    public void supprimerClient(Client client) {
+    public void suppClient(Client client) {
         clients.remove(client);
     }
 }
