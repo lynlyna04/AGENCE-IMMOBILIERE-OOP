c98f32db38494ad78d6a37a2c82243add3a3a402
Lyna Ouabel <88051282+lynlyna04@users.noreply.github.com>
2024-05-23 12:14:00 +0100

    Update Appartement.java

--- src/pack/Appartement.java
+++ src/pack/Appartement.java
@@ -2,32 +2,17 @@ package pack;
 import java.util.Scanner;
 
 public class Appartement extends Propriete {
-    private int etage;
-    private int nbPieces;
 
-    public Appartement(int ID, double superficie, double prix, String localisation, String description, int etage, int nbPieces) {
-        super(ID, TypePropriete.APPARTEMENT, superficie, prix, localisation, description);
-        this.etage = etage;
-        this.nbPieces = nbPieces;
+    public Appartement(int ID, TypePropriete type, double superficie, double prix, String localisation, String description) {
+        super(ID, type, superficie, prix, localisation, description);
     }
 
-    public int getEtage() {
-        return etage;
+    @Override
+    public String toString() {
+        return "ID: " + getID() + ", Type: " + getType() + ", Superficie: " + getSuperficie() + "m², Prix: " + getPrix() + "€, Localisation: " + getLocalisation() + ", Description: " + getDescription();
     }
 
-    public void setEtage(int etage) {
-        this.etage = etage;
-    }
-
-    public int getNbPieces() {
-        return nbPieces;
-    }
-
-    public void setNbPieces(int nbPieces) {
-        this.nbPieces = nbPieces;
-    }
-    
- // Static method to create an Appartement object by taking input from the user
+    // Static method to create an Appartement object by taking input from the user
     public static Appartement createAppartementFromInput() {
         Scanner scanner = new Scanner(System.in);
 
@@ -35,9 +20,10 @@ public class Appartement extends Propriete {
         System.out.print("ID: ");
         int id = scanner.nextInt();
         scanner.nextLine(); // Consume newline character
-        System.out.print("Superficie: ");
+        System.out.print("Type (STUDIO/F2/F3...): ");
+        TypePropriete type = TypePropriete.valueOf(scanner.nextLine().toUpperCase());
+        System.out.print("Superficie (in m²): ");
         double superficie = scanner.nextDouble();
-        scanner.nextLine(); // Consume newline character
         System.out.print("Prix: ");
         double prix = scanner.nextDouble();
         scanner.nextLine(); // Consume newline character
@@ -45,16 +31,9 @@ public class Appartement extends Propriete {
         String localisation = scanner.nextLine();
         System.out.print("Description: ");
         String description = scanner.nextLine();
-        System.out.print("Étage: ");
-        int etage = scanner.nextInt();
-        scanner.nextLine(); // Consume newline character
-        System.out.print("Nombre de pièces: ");
-        int nbPieces = scanner.nextInt();
-        scanner.nextLine(); // Consume newline character
 
-        scanner.close();
+        // Normally, you would also link the property to an agent, but for simplicity, we'll leave it out here
 
-        return new Appartement(id, superficie, prix, localisation, description, etage, nbPieces);
+        return new Appartement(id, type, superficie, prix, localisation, description);
     }
-    
 }
