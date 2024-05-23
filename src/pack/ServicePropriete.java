635b4583aba63d3163756725b8c6402c60442edd
Lyna Ouabel <88051282+lynlyna04@users.noreply.github.com>
2024-05-23 12:17:03 +0100

    Update ServicePropriete.java

--- src/pack/ServicePropriete.java
+++ src/pack/ServicePropriete.java
@@ -1,51 +1,28 @@
 package pack;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class ServicePropriete {
     private List<Propriete> proprietes;
 
-    public ServicePropriete() {
-        this.proprietes = new ArrayList<>();
-    }
-
-    public List<Propriete> getToutesLesProprietes() {
-        return proprietes;
+    public ServicePropriete(List<Propriete> proprietes) {
+        this.proprietes = proprietes;
     }
 
     public Propriete getProprieteParId(int id) {
-        return proprietes.stream()
-                .filter(p -> p.getID() == id)
-                .findFirst()
-                .orElse(null);
+        for (Propriete propriete : proprietes) {
+            if (propriete.getID() == id) {
+                return propriete;
+            }
+        }
+        return null; // Or throw an exception
     }
 
-    public void ajouterPropriete(Propriete propriete) {
+    public void ajoutPropriete(Propriete propriete) {
         proprietes.add(propriete);
     }
 
-    public void modifierPropriete(Propriete propriete) {
-        Propriete proprieteExistante = getProprieteParId(propriete.getID());
-        if (proprieteExistante != null) {
-            proprieteExistante.setType(propriete.getType());
-            proprieteExistante.setSuperficie(propriete.getSuperficie());
-            proprieteExistante.setPrix(propriete.getPrix());
-            proprieteExistante.setLocalisation(propriete.getLocalisation());
-            proprieteExistante.setDescription(propriete.getDescription());
-        }
-    }
-
-    public void supprimerPropriete(Propriete propriete) {
+    public void suppPropriete(Propriete propriete) {
         proprietes.remove(propriete);
-    }
-
-    public List<Propriete> rechercherProprietes(CritereRecherchePropriete criteres) {
-        return proprietes.stream()
-                .filter(p -> p.getPrix() >= criteres.getPrixMin() && p.getPrix() <= criteres.getPrixMax())
-                .filter(p -> criteres.getType() == null || p.getType() == criteres.getType())
-                .filter(p -> criteres.getLocalisation() == null || p.getLocalisation().contains(criteres.getLocalisation()))
-                .collect(Collectors.toList());
     }
 }
